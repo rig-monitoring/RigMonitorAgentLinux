330e4905fde9406ac184fc468638ad7bb8cbee47
Probe <sai@sminers.xyz>
2017-08-26 15:46:35 +0700

    init claymore montor

--- src/main.go
+++ src/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/ybbus/jsonrpc"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func keepLines(s string, n int) string {
+	result := strings.Join(strings.Split(s, "\n")[:n], "\n")
+	return strings.Replace(result, "\r", "", -1)
+}
+
+func getConfig() {
+	return
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	queryMetric()
+	postData()
+}
+
+func postData() {
+	resp, err := http.PostForm("http://localhost:3000/miner",
+		url.Values{"q": {"github"}})
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println("post:\n", keepLines(string(body), 3))
+}
+
+func queryMetric() {
+
+	type ClaymoreMiner struct {
+		Result string `json:"result"`
+	}
+
+	// create client
+	rpcClient := jsonrpc.NewRPCClient("http://localhost:4343")
+
+	// execute rpc to service
+	response, _ := rpcClient.Call("getMinetStat1")
+
+	// parse result into struct
+	var claymore ClaymoreMiner
+	response.GetObject(&claymore)
+
 }
